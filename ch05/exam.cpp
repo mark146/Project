5125234452a44053daa0e54db52afa5ea5d50ee4
marin J <tkdtkdqja@naver.com>
2018-02-18 03:27:22 +0900

    예제

--- ch05/exam.cpp
+++ ch05/exam.cpp
@@ -66,13 +66,99 @@ public:
 
 };
 
+
+
+NamedObj::NamedObj(const char *s) {
+	name = new char[strlen(s) + 1];//문자열을 복사할 공간을 할당
+	strcpy(name, s);
+	id = ++nConstr;//생성된 객체의 수를 증가시키고 이를 ID로 부여
+}
+
+NamedObj::~NamedObj() {
+	++nDestr;//소멸된 객체의 수를 증가시킴.
+	delete[] name;
+}
+
+//static 데이터 멤버의 정의 및 초기화
+int NamedObj::nConstr = 0;
+int NamedObj::nDestr = 0;
+
+
+class test3 {
+public:
+	void f() {
+		NamedObj x("Thrird");//세 번째 객체의 생성
+		x.display();//함수 반환 후 x는 소멸됨.
+	}
+
+	void init() {
+		printf("exam3 \n");
+		cout << "NamedObj 클래스의 객체 수:" << NamedObj::nObj() << endl;
+		NamedObj a("First");//첫 번째 객체 생성
+		NamedObj b("Second");//두 번째 객체 생성
+		a.display();
+		b.display();
+		f();
+		NamedObj c("Fourth");
+		c.display();
+		cout << "NamedObj 클래스의 객체 수:" << NamedObj::nObj() << endl;
+	}
+};
+
+
+
+bool CharStack::push(char ch) {
+	if (chkFull()) {
+		cout << "스택이 가득 차 있습니다." << endl;
+	 return false;
+	}
+	buf[--top] = ch;//스택에 공간이 있으면 저장
+	return true;
+}
+
+char CharStack::pop() {
+	if (chkEmpty()) {
+		cout << "스택에 데이터가 없습니다." << endl;
+		return 0;
+	}
+	return buf[top++];//top 위치의 데이터 반환
+}
+
+
+
+class test4 {
+	CharStack chStack;//20개의 문자를 저장할 수 있는 스택
+	char str[20];
+
+public:
+	void init() {
+		cout << "영어 단어를 입력하시오:";
+		cin >> str;
+
+		char* pt = str;//포인터로 문자열 시작 위치를 가리킴.
+		while (*pt) //문자열의 끝이 아니면 반복
+			chStack.push(*(pt++));//스택에 문자를 넣음.
+		cout << "역순 단어 출력:";
+		while (!chStack.chkEmpty()) //스택이 비어 있지 않으면 반복
+			cout << chStack.pop(); //스택에서 인출한 문자를 출력
+		cout << endl;
+	}
+};
+
+
+
 int main() {//디폴트 private
 	test1 t1;//생성자
 	test2 t2;
-	
+	test3 t3;
+	test4 t4;
+
 	t1.init();
 	t2.init();
+	t3.init();
+	t4.init();
 
+	getchar();
 	getchar();
 
 	return 0;
--- ch05/exam.h
+++ ch05/exam.h
@@ -1,11 +1,8 @@
 #ifndef COUNTER_H_INCLUDED //Counter.h가 중복 include되지 않았는지 검사
-
 #define COUNTER_H_INCLUDED //Counter.h가 처음 include 될 때 정의됨.
-
 class Counter {
 private:
 	int value;
-
 public:
 	Counter() : value(0) { }//초기화 리스트
 
@@ -21,12 +18,11 @@ public:
 		return value;
 	}
 };
-
 #endif
 
 
-#ifndef PERSON_H_INCLUDED //Counter.h가 중복 include되지 않았는지 검사
 
+#ifndef PERSON_H_INCLUDED //Counter.h가 중복 include되지 않았는지 검사
 #define PERSON_H_INCLUDED //Counter.h가 처음 include 될 때 정의됨.
 class Person {
 	char *name;
@@ -39,4 +35,63 @@ public:
 	
 	void chAddr(const char *newAddr);
 };
+#endif
+
+
+
+#ifndef NAMEDOBJ_H_INCLUDED
+#define NAMEDOBJ_H_INCLUDED
+#include <iostream>
+using namespace std;
+class NamedObj {
+	char *name;
+	int id;
+	//static 데이터 멤버
+	static int nConstr;//생성된 객체 수
+	static int nDestr;//소멸된 객체 수
+public:
+	NamedObj(const char *s);//생성자
+	~NamedObj();//소멸자
+
+	void display() const {//객체의 속성 출력
+		cout << "ID : " << id << "--> 이름 : " << name << endl;
+	}
+
+	static int nObj() { return nConstr - nDestr; }//존재하는 객체 수 반환
+
+};
+
+#endif
+
+
+
+#ifndef CHARSTACK_H_INCLUDED
+#define CHARSTACK_H_INCLUDED
+
+class CharStack {
+	enum { size=20};//스택의 크기
+	
+	int top;//마지막 데이터를 가리키는 포인터
+	
+	char buf[size];//스택의 저장공간
+
+public:
+
+	CharStack() { top = size; }//생성자
+	
+	bool chkEmpty() const //스택에 데이터가 없으면 true
+	{
+		return top == size;
+	}
+
+	bool chkFull() const {//스택이 가측 차 있으면 true
+		return !top;
+	}
+
+	bool push(char ch); //스택에 데이터를 넣음.
+
+	char pop();//스택에서 데이터를 꺼냄.
+
+};
+
 #endif
